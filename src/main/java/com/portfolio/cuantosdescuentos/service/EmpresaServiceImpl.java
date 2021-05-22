b876ba4be1cc9d20aa53b5c61a183a5adb481afc
fpuyog <fpuyolg@gmail.com>
2021-05-22 20:32:35 +0200

    Operativo: listado de clientes y empresas; nuevo cliente

--- src/main/java/com/portfolio/cuantosdescuentos/service/EmpresaServiceImpl.java
+++ src/main/java/com/portfolio/cuantosdescuentos/service/EmpresaServiceImpl.java
@@ -3,10 +3,12 @@ package com.portfolio.cuantosdescuentos.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.portfolio.cuantosdescuentos.dao.EmpresaRepository;
 import com.portfolio.cuantosdescuentos.entity.Empresa;
 
+@Service
 public class EmpresaServiceImpl implements EmpresaService {
 	
 	private EmpresaRepository empresaRepository;
